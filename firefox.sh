8a940a3811971cf9a82f00c118013fcdb5bbe2eb
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2023-06-14 20:46:16 +0200

    Create firefox.sh

---
+++ firefox.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Function to display status
+display_status() {
+    echo "Status: $1"
+}
+
+# Start Firefox in full screen mode
+display_status "Starting Firefox in full screen mode..."
+firefox -url http://localhost --kiosk &
+
+# Sleep for a few seconds to allow Firefox to start
+sleep 30
+
+# Check if Firefox is running in full screen
+if pgrep -x "firefox" >/dev/null && wmctrl -l | grep -q "Mozilla Firefox" && xdotool getactivewindow getwindowgeometry | grep -q "Fullscreen"; then
+else
+    display_status "Firefox is not running in full screen. Restarting in 30 seconds..."
+    sleep 30
+    exec "$0" "$@" # Restart the script
+fi
+
+# End the script
+display_status "Script execution complete."
+exit 0
